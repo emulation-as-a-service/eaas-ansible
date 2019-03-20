dbc0e7f9520ffef3bebfc164128d6bf9666aa5d9
Oleg Stobbe <oleg@openslx.com>
2019-03-20 17:04:26 +0100

    fix problems with building base docker-image on macos

--- docker/alpine/build-image.sh
+++ docker/alpine/build-image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 exec docker build --force-rm \
-    --build-arg "uid=$(id --user)" \
-    --build-arg "gid=$(id --group)" \
+    --build-arg "uid=$(id -u)" \
+    --build-arg "gid=$(id -g)" \
     --tag eaas/alpine \
     .
