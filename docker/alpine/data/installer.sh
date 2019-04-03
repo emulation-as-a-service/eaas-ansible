0d9babb05c2bd96e92f77c32d8b4c940ca157404
Oleg Stobbe <oleg@openslx.com>
2019-04-03 20:41:37 +0200

    simplify host/container user mapping in base docker-image

--- docker/alpine/data/installer.sh
+++ docker/alpine/data/installer.sh
@@ -2,30 +2,13 @@
 
 set -e  # fail on error
 
-__uid2name() {
-    getent passwd "$1" | cut -d : -f 1
-}
-
-__gid2name() {
-    getent group "$1" | cut -d : -f 1
-}
-
-
 name="${1:?}"
 uid="${2:?}"
 gid="${3:?}"
 
-# delete exisiting user with target uid
-curname="$(__uid2name "${uid}")"
-if [ -n "${curname}" ] ; then
-    deluser "${curname}"
-fi
-
-# delete exisiting group with target gid
-curname="$(__gid2name "${gid}")"
-if [ -n "${curname}" ] ; then
-    delgroup "${curname}"
-fi
+# reset user/group DB to contain only the root user/group
+echo 'root:x:0:0:root:/root:/bin/ash' > /etc/passwd
+echo 'root:x:0:root' > /etc/group
 
 # create a new container's user/group matching a host's UID/GID
 addgroup -g "${gid}" "${name}"
