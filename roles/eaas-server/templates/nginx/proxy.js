f77198a94282fd75cd078594e7fddd14cba08f98
Klaus Rechert <klaus@openslx.com>
2019-10-26 12:46:46 +0200

    new nginx / docker-compose

---
+++ roles/eaas-server/templates/nginx/proxy.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var fs = require("fs");
+var crypto = require("crypto");
+var atob = s => String.bytesFrom(s, "base64");
+var hash = s => crypto.createHash("sha256").update(s).digest("hex");
+
+function imageId(r)
+{
+	var uri = r.variables.request_uri;
+  	var ids = uri.split("/");
+ // r.log("ID: ");
+ // r.log(ids[ids.length - 1]);
+	return ids[ids.length - 1];
+}
+
+function token(r)
+{
+  var auth = r.headersIn['Authorization'];
+  var auth2 = atob(auth.split(/\s+/)[1]).match(/(.*?):(.*)/).slice(1);
+  if (auth2[0] !== "jwt") throw undefined;
+  var token = "Bearer " + auth2[1];
+  // r.log("token: " + token);
+
+  return token;
+}
+
+function destURL(r) {
+  var uri = r.variables.request_uri;
+  // r.log("destUrl ");
+  // r.log(uri.match(/\/(https?:\/\/.+)$/)[1]);
+  return uri.match(/\/(https?:\/\/.+)$/)[1];
+}
+
