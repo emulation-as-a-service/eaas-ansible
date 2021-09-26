cd469a76bb5aa33a9f498d7c7cbff75a50016b88
Oleg Stobbe <oleg@openslx.com>
2021-09-26 18:49:20 +0200

    Add support for running arbitrary pre/post-start hooks

---
+++ roles/eaas-server/templates/hooks/runner.sh
@@ -0,0 +1,25 @@
+#!/bin/env sh
+
+set -eu
+
+kind="$1"
+
+__info() {
+    echo "[HOOKS] $1"
+}
+
+workdir="$(dirname -- "$0")/${kind}"
+if [ ! -d "${workdir}" ]; then
+    __info "No ${kind} hooks found!"
+    exit 0
+fi
+
+cd "${workdir}"
+
+__info "Running ${kind} hooks..."
+for script in $(ls ./); do
+    __info "--> ${script}"
+    ./${script}
+done
+
+__info "Running ${kind} hooks finished!"
